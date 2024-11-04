b75179ac2072669cfd2e7ec39cbe2102277ea02f
Julian <julianherbert.2007@gmail.com>
2024-11-04 07:25:03 -0500

    replace card deck with spritesheet

--- Main.java
+++ Main.java
@@ -1,48 +1,33 @@
-import javax.swing.*;
 
-import java.awt.*;
+/**
+ * Write a description of class Main here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Main
+{
+    // instance variables - replace the example below with your own
+    private int x;
 
-import java.io.File;
-
-import java.util.Arrays;
-
-class Main {
-    public static final int CARD_WIDTH = 207;
-    public static final int CARD_HEIGHT = 284;
-    
-    public static Image[] cardImages = new Image[53];
-    public static Card[] cards = new Card[53];
-    
-    private static void error(String message) {
-        JOptionPane.showMessageDialog(null, message, "Error", JOptionPane.ERROR_MESSAGE);
+    /**
+     * Constructor for objects of class Main
+     */
+    public Main()
+    {
+        // initialise instance variables
+        x = 0;
     }
-    
-    private static void fatalError(String message) {
-        error(message);
-        System.exit(1);
-    }
-    
-    public static void main(String[] args) {
-        // Gather all card images
-        File cardFolder = new File("cards/");
-        if (cardFolder.exists()) {
-            File[] cardFiles = cardFolder.listFiles();
-            
-            // Sort card files
-            // TBD
-            
-            for (int i=0; i < cards.length; i++) {
-                cardImages[i] = new ImageIcon(cardFiles[i].getPath()).getImage().getScaledInstance(Main.CARD_WIDTH, Main.CARD_HEIGHT, Image.SCALE_SMOOTH);
-            }
-        } else {
-            fatalError("No cards were found! Make sure you have all the images in the cards folder.");
-        }
-        
-        // Now the cards can be displayed
-        Point initialLocation = new Point(50, 50);
-        for (int i=0; i < cardImages.length; i++) {
-            cards[i] = new Card(cardImages[i]);
-            cards[i].setLocation(initialLocation);
-        }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
     }
 }
